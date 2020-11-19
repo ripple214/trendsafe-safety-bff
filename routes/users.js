de318a4c0be0b3a1ffc6d98f97347773d8661771
Rigie Acebedo <rigie.acebedo@carlsonwagonlit.com>
2020-11-19 18:45:18 +0800

    Added clients endpoints

--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/login", (req, res, next) => {  
-  if(req.body.username == 'CWLH' && req.body.password == 'Singapore') {
+  if(req.body.username == 'admin' && req.body.password == 'Singapore') {
     var response = {
       accessToken: uuid.v4(),
       module: "ADMIN"
     };
   
     res.json(response);
-  } else if(req.body.username == 'client' && req.body.password == 'password') {
+  } else if(req.body.username == 'client' && req.body.password == 'Singapore') {
     var response = {
       accessToken: uuid.v4(),
       module: "CLIENT"
