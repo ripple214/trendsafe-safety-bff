6ebc09a011217d7f1d67b5fee935ddbe9aeaa943
ripple214 <acebedoro@yahoo.com>
2021-02-28 22:02:26 +0800

    set env

--- app.js
+++ app.js
@@ -69,6 +69,27 @@ app.all('/*', (req, res, next) => {
   next();
 });
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', conf.get('BFF_URL'));
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
