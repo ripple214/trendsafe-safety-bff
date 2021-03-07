86f6cfd3b38ad61e260c0f39f0190894820f248c
ripple214 <acebedoro@yahoo.com>
2021-03-07 21:08:47 +0800

    Corrected cors whitelisting

--- app.js
+++ app.js
@@ -90,29 +90,34 @@ const authenticateJWT = (req, res, next) => {
 
   //req.user = {clientId: 'dummy-client', emailAddress: 'client'}; next(); //TODO remove these once ssl cert becomes available
 
-  const authorization = getAppCookies(req, res)['Authorization'];
-  console.log("authorization is ", authorization);
-  if (authorization) {
-      jwt.verify(authorization, ACCESS_TOKEN_SECRET, (err, user) => {
-          if (err) {
-              return res.sendStatus(403);
-          }
-
-          req.user = user;
-
-          let response = {
-            sessionId: user.sessionId,
-            clientId: user.clientId,
-            emailAddress: user.emailAddress,
-            module: user.module
-          };
-          let accessToken = jwt.sign(response, ACCESS_TOKEN_SECRET, {expiresIn: "30m"});
-          res.setHeader('Set-Cookie', 'Authorization=' + accessToken + '; HttpOnly; Path=/; SameSite=Lax;');
-          
-          next();
-      });
+  console("path is", req.path);
+  if(req.path.startsWith("/auth")) {
+    next();
   } else {
-      res.sendStatus(401);
+    const authorization = getAppCookies(req, res)['Authorization'];
+    console.log("authorization is ", authorization);
+    if (authorization) {
+        jwt.verify(authorization, ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+  
+            req.user = user;
+  
+            let response = {
+              sessionId: user.sessionId,
+              clientId: user.clientId,
+              emailAddress: user.emailAddress,
+              module: user.module
+            };
+            let accessToken = jwt.sign(response, ACCESS_TOKEN_SECRET, {expiresIn: "30m"});
+            res.setHeader('Set-Cookie', 'Authorization=' + accessToken + '; HttpOnly; Path=/; SameSite=Lax;');
+            
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
   }
 };
 
